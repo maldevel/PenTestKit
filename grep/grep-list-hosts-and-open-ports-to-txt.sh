05e14bdfe8e3c79f8aa6402e6bf18035074b82a9
maldevel <maldevel@mail.com>
2020-03-11 16:51:36 +0200

    export hosts and open ports to txt

---
+++ grep/grep-list-hosts-and-open-ports-to-txt.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#    This file is part of PenTestKit
+#    Copyright (C) 2017-2020 @maldevel
+#    https://github.com/maldevel/PenTestKit
+#
+#    PenTestKit - Useful tools for Penetration Testing.
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#    For more see the file 'LICENSE' for copying permission.
+
+if [ $# -eq 1 ]; then
+
+  egrep -v "^#|Status: Up" $1/*.gnmap|cut -d' ' -f2,4-| sed 's/Ignored.*//g' | awk '{printf $1 ";" NF-1 ";"; $1=""; for(i=2; i<=NF; i++) { a=a""$i; }; split(a,s,","); for(e in s) { split(s[e],v,"/"); printf "%s(%s),", v[1], v[5]}; a=""; printf "\n"; }'
+
+else
+  echo "Please provide a directory path."
+fi
